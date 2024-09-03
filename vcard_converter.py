5760f99d529c88ec145adedd25cc46ae20d70411
Jordan Allen <land.of.jordan@gmail.com>
2024-09-03 12:02:34 -0500

    script changes for vercel

--- vcard_converter.py
+++ vcard_converter.py
@@ -1,26 +1,39 @@
+from http.server import BaseHTTPRequestHandler
 import pandas as pd
-import os
+import zipfile
+from io import BytesIO
 
-# Load the CSV file
-csv_file_path = '/Users/jordanallen/Desktop/contacts.csv'
-df = pd.read_csv(csv_file_path)
 
-# Directory to save the .vcf files
-output_directory = os.path.dirname(csv_file_path)
+class handler(BaseHTTPRequestHandler):
 
-for index, row in df.iterrows():
-    # Start the vCard content
-    vcard = f"""BEGIN:VCARD
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+
+        # Read CSV from POST data
+        df = pd.read_csv(BytesIO(post_data))
+
+        # Create ZIP file in memory
+        memory_file = BytesIO()
+        with zipfile.ZipFile(memory_file, 'w') as zf:
+            for index, row in df.iterrows():
+                vcard = f"""BEGIN:VCARD
 VERSION:3.0
 N:{row['Last Name']};{row['First Name']}
 FN:{row['First Name']} {row['Last Name']}
 TEL;TYPE=CELL:{row['Phone Number']}
 EMAIL:{row['Email Address']}
 END:VCARD
-"""
+                """
+                zf.writestr(
+                    f"{row['First Name']}_{row['Last Name']}.vcf", vcard)
 
-    # Save to a .vcf file in the same directory as the CSV file
-    file_name = f"{row['First Name']}_{row['Last Name']}.vcf"
-    file_path = os.path.join(output_directory, file_name)
-    with open(file_path, "w") as file:
-        file.write(vcard)
+        memory_file.seek(0)
+        zip_content = memory_file.read()
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/zip')
+        self.send_header('Content-Disposition',
+                         'attachment; filename="vcards.zip"')
+        self.end_headers()
+        self.wfile.write(zip_content)
